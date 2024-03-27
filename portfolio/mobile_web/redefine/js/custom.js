b3c4cec77489d93520d93254530eb0c3ab3b505d
jongju-Lee <jeje7@naver.com>
2024-03-27 15:09:00 +0900

    redefine fixing

--- portfolio/mobile_web/redefine/js/custom.js
+++ portfolio/mobile_web/redefine/js/custom.js
@@ -8,12 +8,12 @@ $(function () {
   $("footer").load("../include/gnb.html");
   /* ##### Register - Terms CheckBox ##### */
   $("input[id=agree_all]").click(function () {
-    // 전체선택 체크박스 체크시
+    // 전체선택 체크박스 체크 되면
     if ($("input[id=agree_all]").prop("checked")) {
-      // 전체 체크
+      // 전체 체크박스 체크
       $("input[name=agree]").prop("checked", true);
     } else {
-      // 전체 체크 해제
+      // 전체 체크박스 체크 해제
       $("input[name=agree]").prop("checked", false);
     }
   });
